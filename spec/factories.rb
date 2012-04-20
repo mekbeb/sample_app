c379f6624610ac4016641bc81a255ba6e949e5ff
Mekbeb Worku <mekbeb@gmail.com>
2012-04-19 22:15:34 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,13 @@ Factory.define :user do |user|
 	user.password_confirmation  "foobar"
 end
 
+Factory.sequence :name do |n|
+	"Person #{n}"
+end
+
+Factory.sequence :email do |n|
+	"person-#{n}@example.com"
+end
+
+
 
